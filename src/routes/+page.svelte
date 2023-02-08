6ba01ff21922559e3f246ebfbfce640ab7a71330
Faris <faris.mecinovic.np@gmail.com>
2023-02-08 16:23:14 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Todo from '../components/Todo.svelte';
+	import TodoForm from '../components/TodoForm.svelte';
+	import { todos } from '../stores/todosStore';
+</script>
+
+<div>
+	<h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">My Todos</h1>
+	<TodoForm />
+	{#each $todos as todo}
+		<Todo {todo} />
+	{/each}
+</div>
